0b96b2d879260185bfebd4ed3f1c0972629de207
Armoru <rmoru31@gmail.com>
2018-02-22 20:01:18 +0700

    Kagami and add help

--- index.js
+++ index.js
@@ -95,8 +95,10 @@ bot.on('message',(message)=>{
 		message.channel.send("Wooing Sena :ballot_box_with_check:\nRich boye :ballot_box_with_check:\nRaising disappointment because cute :ballot_box_with_check:\nWants all the cute grills :ballot_box_with_check:\nChanging IGN depending on mood :ballot_box_with_check:\nWeeb room :ballot_box_with_check:\nSamurais :ballot_box_with_check: \nO MEGHAAA weebs :ballot_box_with_check: \nBuying figurines w/o looking at price :ballot_box_with_check: \nMUST BE NEO");
 	}else if(message.content == '!moru'){
 		message.channel.send("Never sleeps :ballot_box_with_check:\nShut in n33t :ballot_box_with_check:\n8,000 hours of dota :ballot_box_with_check:\nFockin weeb :ballot_box_with_check:\n Betraying people :ballot_box_with_check: \nHuge M :ballot_box_with_check: \nBiased :ballot_box_with_check: \n MUST BE MORU");
+	}else if(message.content == '!kagami'){
+		message.channel.send("League all day :ballot_box_with_check: \nStays inside :ballot_box_with_check: \nSmol wrists :ballot_box_with_check: \nLikes to be dominated :ballot_box_with_check: \nDangerous :ballot_box_with_check: \nMakes YouTube videos :ballot_box_with_check: \nLit af :ballot_box_with_check: \nKekekek :ballot_box_with_check: \nMeta whore :ballot_box_with_check: \nCan control ppl :ballot_box_with_check: \nLike thicc :ballot_box_with_check: \nLikes lewd dominant women :ballot_box_with_check: \nCooks SPAGHETT :ballot_box_with_check: \nSpends money on cardboards :ballot_box_with_check:\nDed inside :ballot_box_with_check: \nReady to fite :ballot_box_with_check:\nToxic af :ballot_box_with_check: \nSavage :ballot_box_with_check: \nMUST BE KEKGAMI");
 	}else if(message.content == '!help'){
-		message.channel.send("Say ``!pull`` for a disappointment\n``!gm`` for gm facts\n``!sena`` for sena facts\n``!cyro`` for cyro facts\n``!flail`` for flail facts\n``!neo`` for neo facts");
+		message.channel.send("Say ``!pull`` for a disappointment\n``!gm`` for gm facts\n``!sena`` for sena facts\n``!cyro`` for cyro facts\n``!flail`` for flail facts\n``!neo`` for neo facts\n``!moru`` for moru facts\n``!kagami`` for kagami facts");
 	}else if(message.content == '!satan'){
 		message.reply("Hi dipshit");
 	}else if (message.content =='!testfor'){
